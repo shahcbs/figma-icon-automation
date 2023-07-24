7581bc7c818b2d7950c96e4274ef17e9651a36b5
Shah <shah.ali@cbsinteractive.com>
2023-07-24 11:41:24 -0400

    added int files

---
+++ fetch.js
@@ -0,0 +1,17 @@
+const figmaApiExporter = require('figma-api-exporter').default;
+
+const exporter = figmaApiExporter(figd_gVD0cGld4QfB7xI1udcBNCKH2aMnFsY8ltlyfWmm);
+
+exporter
+  .getSvgs({
+    fileId: ICZQcuK2xzGmQpTddTVU2a,
+    canvas: 'Icons',
+  })
+  .then(async svgsData => {
+    await exporter.downloadSvgs({
+      saveDirectory: './figmaIcons',
+      svgsData: svgsData.svgs,
+      lastModified: svgsData.lastModified,
+      clearDirectory: true,
+    });
+  });
